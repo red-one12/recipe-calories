3dbdcd1262a642b4f161bd84911dce23788ee753
red-one12 <radoanahmed1242000@gmail.com>
2024-10-24 13:31:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+        <p className='text-4xl text-emerald-700 font-bold'>Hello</p>
+      
+    </>
+  )
+}
+
+export default App
